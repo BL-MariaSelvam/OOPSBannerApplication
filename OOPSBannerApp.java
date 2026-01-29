3452d8ab7b96111d91928bb049a984f6719c7b14
BL-MariaSelvam <maria.selvam@bridgelabz.com>
2026-01-29 15:22:24 +0530

    [Maria] Added: UC5-Render OOPs as Banner using Inline Array Initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,49 @@
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+         String[][] letters = {
+                {   // O
+                        " *****  ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        " *****  "
+                },
+                {   // O
+                        " *****  ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        " *****  "
+                },
+                {   // P
+                        " *****  ",
+                        "*     * ",
+                        "*     * ",
+                        " *****  ",
+                        "*        ",
+                        "*        ",
+                        "*        "
+                },
+                {   // S
+                        " *****  ",
+                        "*        ",
+                        "*        ",
+                        " *****  ",
+                        "       *",
+                        "       *",
+                        " *****  "
+                }
+        };
+
+        for (int row = 0; row < 7; row++) {
+            for (String[] letter : letters) {
+                System.out.print(letter[row] + " ");
+            }
+            System.out.println();
+        }
     }
 }
