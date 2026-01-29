aaaa2643f89bda3e2b3af054a45a57f9a39709b3
BL-MariaSelvam <maria.selvam@bridgelabz.com>
2026-01-29 15:42:04 +0530

    [Maria] Added: UC6-Render OOPs as Banner using Functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,58 @@
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String[][] banner = getOOPSBanner();
+        printBanner(banner);
+    }
+
+    // Returns banner data
+    private static String[][] getOOPSBanner() {
+        return new String[][]{
+                {   // O
+                        " *****  ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        " *****  "
+                },
+                {   // O
+                        " *****  ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        "*     * ",
+                        " *****  "
+                },
+                {   // P
+                        " *****  ",
+                        "*     * ",
+                        "*     * ",
+                        " *****  ",
+                        "*        ",
+                        "*        ",
+                        "*        "
+                },
+                {   // S
+                        " *****  ",
+                        "*        ",
+                        "*        ",
+                        " *****  ",
+                        "       *",
+                        "       *",
+                        " *****  "
+                }
+        };
+    }
+
+    // Prints the banner
+    private static void printBanner(String[][] letters) {
+        for (int row = 0; row < 7; row++) {
+            for (String[] letter : letters) {
+                System.out.print(letter[row] + " ");
+            }
+            System.out.println();
+        }
     }
 }
